21000e9fbefb2254a1df293e4d6cfe150198409e
Dmytro Bogush <qimchik@mail.ru>
2016-11-01 12:45:11 +0300

    markup

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function onclick(e){
+	if (e.target.tagName === 'DIV') return false;
+	if (e.target.innerHTML === '=') {
+		calculate();
+		return false;
+	}
+	if (e.target.innerHTML === '.') {
+	if (document.getElementById('monitor').innerHTML.indexOf('.')===-1) 
+		document.getElementById('monitor').innerHTML += '.';	
+		return false;
+	}
+	//console.log(document.getElementById('monitor') == '0');
+	if (document.getElementById('monitor').innerHTML == '0') 
+		document.getElementById('monitor').innerHTML = e.target.innerHTML;
+	else document.getElementById('monitor').innerHTML += e.target.innerHTML;
+}
+
+
+
+
+
+
+
+
+
+
+document.getElementById('panel').addEventListener('click', onclick);
